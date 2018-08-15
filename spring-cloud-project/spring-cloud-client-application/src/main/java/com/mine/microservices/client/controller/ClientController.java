2c6082648a21d428f1713a398c9babc1942447b5
刘海飞 <15901995342@163.com>
2018-08-15 16:50:54 +0800

    微服务demo

--- spring-cloud-project/spring-cloud-client-application/src/main/java/com/mine/microservices/client/controller/ClientController.java
+++ spring-cloud-project/spring-cloud-client-application/src/main/java/com/mine/microservices/client/controller/ClientController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -29,10 +30,9 @@ public class ClientController {
 
     //自定义restTemplate
     @Autowired
+    @LoadBalanced
     private RestTemplate restTemplate;
-    @Autowired
-    private ClientHttpRequestInterceptor clientHttpRequestInterceptor;
-    @Value("${spring.application.name}")
+
     private String currentServiceName;
 
 //    @Scheduled(fixedRate=10*1000) //每10秒更新一次
@@ -52,11 +52,12 @@ public class ClientController {
     @GetMapping("/invoke/{serviceName}/say")
     public String invokeSay(@PathVariable String serviceName,
                             @RequestParam String message){
-        restTemplate.setInterceptors(Arrays.asList(clientHttpRequestInterceptor));
 
         return restTemplate.getForObject("/"+serviceName+"/say?message="+message,String.class);
     }
     @Bean
+    @Autowired
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
@@ -64,5 +65,15 @@ public class ClientController {
     @Bean
     public ClientHttpRequestInterceptor interceptor(){
         return new LoadBanlancedRequestInterceptor();
+    }
+
+    @Bean
+    @Autowired
+    public Object customizer(@LoadBalanced Collection<RestTemplate> restTemplates,
+                             ClientHttpRequestInterceptor interceptor) {
+        restTemplates.forEach(r -> {
+            r.setInterceptors(Arrays.asList(interceptor));
+        });
+        return new Object();
     }
 }
